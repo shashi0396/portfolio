309865ec1c5a9363992ec076b9fe841bcca81c5c
shashi0396 <shashikirandc1315@gmail.com>
2026-01-18 09:31:36 +0530

    Initial Next.js portfolio

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next'
+
+const repoName = 'portfolio'
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
